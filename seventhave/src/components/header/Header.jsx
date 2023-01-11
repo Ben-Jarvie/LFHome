343945de2e6962dc045cd8894204b786b926a763
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2023-01-11 13:12:39 -0600

    adding components initial files

---
+++ seventhave/src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./header.css";
+
+const About = () => {
+    return (
+
+    );
+};
+
+export default About;
---
+++ seventhave/src/components/services/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./services.css";
+
+const About = () => {
+    return (
+
+    );
+};
+
+export default Services;
