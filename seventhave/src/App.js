eca589a361705a3cf01fb4a73e1937866af7e050
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2023-01-18 13:16:42 -0600

    replicated carousel section for about us pictures and descriptions

--- seventhave/src/App.js
+++ seventhave/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from "./components/header/Header";
 import About from "./components/about/About";
-// import Gallery from './components/gallery/Gallery';
+import Gallery, { GalleryItem } from './components/gallery/Gallery';
 import Carousel, { CarouselItem } from "./components/carousel/Carousel";
 import Services from "./components/services/Services";
 import Footer from "./components/footer/Footer"
@@ -13,12 +13,17 @@ const App = () => {
     <div>
       <Header/>
       <About/>
+      <Gallery>
+        <GalleryItem>Item 1</GalleryItem>
+        <GalleryItem>Item 2</GalleryItem>
+        <GalleryItem>Item 3</GalleryItem>
+      </Gallery>
+      <Services/>
       <Carousel>
         <CarouselItem>Item 1</CarouselItem>
         <CarouselItem>Item 2</CarouselItem>
         <CarouselItem>Item 3</CarouselItem>
       </Carousel>
-      <Services/>
       <Footer/>
     </div>
   );
--- seventhave/src/components/gallery/Gallery.js
+++ seventhave/src/components/gallery/Gallery.js
@@ -1,21 +1,102 @@
-import React from 'react';
-import "./gallery.css";
-import Carousel, { CarouselItem } from "./components/carousel/Carousel";
+import React, { useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeftLong, faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 
-const Gallery = () => {
-    return (
-        <div className='gallerymain'>
-           
-           <div className='Carouselshell'>
-                <Carousel>
-                    <CarouselItem>Item 1</CarouselItem>
-                    <CarouselItem>Item 2</CarouselItem>
-                    <CarouselItem>Item 3</CarouselItem>
-                </Carousel>
-           </div>
-        </div>
-    );
+import "./gallery.css";
+
+export const GalleryItem = ({ children, width }) => {
+  return (
+    <div className="gallery-item" style={{ width: width }}>
+      {children}
+    </div>
+  );
 };
 
+const Gallery = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [paused, setPaused] = useState(false);
+
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = React.Children.count(children) - 1;
+    } else if (newIndex >= React.Children.count(children)) {
+      newIndex = 0;
+    }
+
+    setActiveIndex(newIndex);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!paused) {
+        updateIndex(activeIndex + 1);
+      }
+    }, 3000);
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  });
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => updateIndex(activeIndex + 1),
+    onSwipedRight: () => updateIndex(activeIndex - 1)
+  });
+
+  return (
+    <div
+      {...handlers}
+      className="gallery"
+      onMouseEnter={() => setPaused(true)}
+      onMouseLeave={() => setPaused(false)}
+    >
+      <div
+        className="galleryinner"
+        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+      >
+        {React.Children.map(children, (child, index) => {
+          return React.cloneElement(child, { width: "100%" });
+        })}
+    </div>
+    <div className="arrowsandindicators">
+      <div className="prevbuttoncontainer">
+            <button 
+                className="prevbutton"
+                onClick={() => {
+                    updateIndex(activeIndex - 1);
+                }}>
+                <FontAwesomeIcon icon={faArrowLeftLong} className="leftarrow"/>
+            </button>
+        </div>
+        <div className="indicators">
+        {React.Children.map(children, (child, index) => {
+          return (
+            <button
+              className={`${index === activeIndex ? "active" : ""}`}
+              onClick={() => {
+                updateIndex(index);
+              }}
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+      </div>
+        <div className="nextbuttoncontainer">
+      <button 
+                className="nextbutton"
+                onClick={() => {
+                    updateIndex(activeIndex + 1);
+                }}>
+                <FontAwesomeIcon icon={faArrowRightLong} className="rightarrow"/>
+            </button>
+        </div>
+    </div>
+</div>
+  );
+};
 
 export default Gallery;
