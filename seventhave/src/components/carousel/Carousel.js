b6ff19ffa73f462a39be86cd942f8c02dbca6ddc
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2023-01-18 11:06:40 -0600

    adding if active logic

--- seventhave/src/components/carousel/Carousel.js
+++ seventhave/src/components/carousel/Carousel.js
@@ -12,9 +12,20 @@ export const CarouselItem = ({children, width}) => {
 const Carousel = ({ children }) => {
     const [activeIndex, setActiveIndex] = useState(0);
 
+    const updateIndex = (newIndex) => {
+        if (newIndex < 0) {
+            newIndex = 0;
+        } else if (newIndex >= React.Children.count(children)) {
+            newIndex = React.Children.count(children) - 1;
+        }
+    }
+
     return (
         <div className='carousel'>
-            <div className='inner' style={{transform: "translateX(-${activeIndex * 100}%)"}}>
+            <div 
+                className='inner' 
+                style={{transform: "translateX(-${activeIndex * 100}%)"}}
+            >
                 {React.Children.map(children, (child, index) => {
                     return React.cloneElement(child, { width: "100%"});
                 })}
