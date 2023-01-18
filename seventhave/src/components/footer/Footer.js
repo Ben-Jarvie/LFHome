4e4c55affbde30e5d4ed09ad3e3a40078fead372
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2023-01-17 18:28:09 -0600

    started styling the footer

--- seventhave/src/components/footer/Footer.js
+++ seventhave/src/components/footer/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import "./footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import Logo from "../../img/logo.png";
 
 const Footer = () => {
     return (
@@ -8,6 +12,21 @@ const Footer = () => {
                 Our Mission: To model Jesus' love by creating
 life-enhancing relationships, services, and environments.
             </h3>
+            <div className="footercontact">
+      <h2 className="LFH">Love, Hope and Faith Home</h2>
+        <div className='footersocialmediaicons'>
+          <a href="https://www.facebook.com/profile.php?id=100089494389145">
+          <FontAwesomeIcon icon={faFacebook} className="footerfacebook"/>
+          </a>
+          <a href="https://www.instagram.com/hope_love_and_faith_homes/">
+          <FontAwesomeIcon icon={faInstagram} className="footerinstagram"/>
+          </a>
+          <a href="mailto:dawn@lovehopefaithhomes.com">
+          <FontAwesomeIcon icon={faEnvelope} className="footeremail"/>
+          </a>
+      </div>
+      </div>
+      <div className='footertext'>
             <p>
                 5595 County Road Z, West Bend, WI 53095 | (262) 306-2100
             </p>
@@ -16,7 +35,9 @@ life-enhancing relationships, services, and environments.
             </p>
             <div>
                 <a>Terms and Conditions</a>
+                <span> </span>
                 <a>Privacy Policy</a>
+            </div>
             </div>
         </div>
     );
