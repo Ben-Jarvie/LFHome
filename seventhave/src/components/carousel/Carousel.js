a7aaa2a9d7a7b9f4ec873b6a7584e94138a79663
Ben Jarvie <112665215+LambdaDev22@users.noreply.github.com>
2023-01-18 11:28:34 -0600

    carousel built, just needs to be styled

--- seventhave/src/components/carousel/Carousel.js
+++ seventhave/src/components/carousel/Carousel.js
@@ -1,54 +1,96 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
+
 import "./carousel.css";
 
-export const CarouselItem = ({children, width}) => {
-    return (
-        <div className='carousel-item' style={{width: width}}>
-            {children}
-        </div>
-    );
+export const CarouselItem = ({ children, width }) => {
+  return (
+    <div className="carousel-item" style={{ width: width }}>
+      {children}
+    </div>
+  );
 };
 
 const Carousel = ({ children }) => {
-    const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [paused, setPaused] = useState(false);
 
-    const updateIndex = (newIndex) => {
-        if (newIndex < 0) {
-            newIndex = 0;
-        } else if (newIndex >= React.Children.count(children)) {
-            newIndex = React.Children.count(children) - 1;
-        }
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = React.Children.count(children) - 1;
+    } else if (newIndex >= React.Children.count(children)) {
+      newIndex = 0;
     }
 
-    return (
-        <div className='carousel'>
-            <div 
-                className='inner' 
-                style={{transform: "translateX(-${activeIndex * 100}%)"}}
-            >
-                {React.Children.map(children, (child, index) => {
-                    return React.cloneElement(child, { width: "100%"});
-                })}
-            </div>
-            <div className='indicators'>
-                <button 
-                    onClick={() => {
-                        updateIndex(activeIndex - 1);
-                    }}
-                >
-                    Prev
-                </button>
-                <button
-                    onClick={() => {
-                        updateIndex(activeIndex + 1);
-                    }}
-                >
-                    Next
-                </button>
-            </div>
-        </div>
-    );
-};
+    setActiveIndex(newIndex);
+  };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!paused) {
+        updateIndex(activeIndex + 1);
+      }
+    }, 3000);
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  });
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => updateIndex(activeIndex + 1),
+    onSwipedRight: () => updateIndex(activeIndex - 1)
+  });
+
+  return (
+    <div
+      {...handlers}
+      className="carousel"
+      onMouseEnter={() => setPaused(true)}
+      onMouseLeave={() => setPaused(false)}
+    >
+      <div
+        className="inner"
+        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+      >
+        {React.Children.map(children, (child, index) => {
+          return React.cloneElement(child, { width: "100%" });
+        })}
+      </div>
+      <div className="indicators">
+        <button
+            className="prevbutton"
+            onClick={() => {
+                updateIndex(activeIndex - 1);
+          }}
+        >
+          Prev
+        </button>
+        {React.Children.map(children, (child, index) => {
+          return (
+            <button
+              className={`${index === activeIndex ? "active" : ""}`}
+              onClick={() => {
+                updateIndex(index);
+              }}
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+        <button
+            className="nextbutton"  
+            onClick={() => {
+                updateIndex(activeIndex + 1);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
